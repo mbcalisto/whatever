e3f0a1a9ffb98a83ddcc4c513b5a47dc0a6f683b
Calisto <mateus.bruno.calisto@gmail.com>
2022-03-22 15:59:13 -0300

    Back-End

---
+++ ocinator/fakeDatabase.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class suggestions {
+    let choices: Double
+    init(choices: Double) {
+        self.choices = choices
+    }
+}
+
+let op1 = suggestions(choices: 1111111111)
+//let op1 = suggestions(choices: 1111111111)
+//let op2 = suggestions(choices: 1111111112)
+//let op3 = suggestions(choices: 1111111113)
+//let op4 = suggestions(choices: 1111111114)
+
+
+
+let suggestions = [op1]
+
+//, op2, op3, op4]
--- ocinator/main.swift
+++ ocinator/main.swift
@@ -1,56 +1,30 @@
 import Foundation
 
-print("""
-Welcome to Ocinator!
-Your Leisure Choice Automation!
-Tell me your Name:
-""")
+// IMPORT FAKE DATABASE
 
-let name = readLine()
-
-print("Hi! \(name!).")
-print("Start choosing a number of what you want and I will say what you wish!")
-
-print("""
-At First, tell me what you wish:
-1 - I want EAT!
-2 - I want DRINK!
-3 - I want DANCE!
-""")
-
-let choose = Int(readLine(strippingNewline: true)!)!
-
-//============================================================================================
-
-if choose == 1 {
-    print("""
-    You want to eat, hun? So just a few more questions...
-    What do you wish...
-    1 - I wish a new place!
-    2 - Nah! I want same food!
-    """)
-    let ea = Int(readLine(strippingNewline: true)!)!
-        if ea == 1 {
-            print("""
-            A new place? So let's make quicky..
-            1 - I wish pastas! (Pizza
-            2 - I wish truck food!
-            """)
-        }
-    
-        if ea == 2 {
-            print("b")
-        }
+if let filepath = Bundle.main.path(forResource: "testFile", ofType: "swift") {
+    do {
+        let contents = try String(contentsOfFile: filepath)
+        print(contents)
+    }
 }
 
-//============================================================================================
+let a = Int(readLine(strippingNewline: true)!)!
+let b = Int(readLine(strippingNewline: true)!)!
+let c = Int(readLine(strippingNewline: true)!)!
+let d = Int(readLine(strippingNewline: true)!)!
+let e = Int(readLine(strippingNewline: true)!)!
+let f = Int(readLine(strippingNewline: true)!)!
+let g = Int(readLine(strippingNewline: true)!)!
+let h = Int(readLine(strippingNewline: true)!)!
+let i = Int(readLine(strippingNewline: true)!)!
+let j = Int(readLine(strippingNewline: true)!)!
 
-if choose == 2 {
-    print("You want drink, hun? So just a few more questions...")
+var z = Int("\(a)\(b)\(c)\(d)\(e)\(f)\(g)\(h)\(i)\(j)")
+
+let x = 0
+
+if students.contains(z!) {
+    print(students)
 }
 
-//============================================================================================
-
-if choose == 3 {
-    print("So you want to dance? Shake it off for a few more questions...")
-}
