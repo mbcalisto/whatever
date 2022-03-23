593e52e1661e04345fb217797302ba7b5276abb7
Calisto <mateus.bruno.calisto@gmail.com>
2022-03-23 09:50:25 -0300

    Final Back-End

--- ocinator/fakeDatabase.swift
+++ ocinator/fakeDatabase.swift
@@ -1,20 +1,44 @@
 import Foundation
 
-class suggestions {
-    let choices: Double
-    init(choices: Double) {
-        self.choices = choices
-    }
+class questions {
+    let q01 : String = ""
+    let q02 : String = ""
+    let q03 : String = ""
+    let q04 : String = ""
+    let q05 : String = ""
+    let q06 : String = ""
+    let q07 : String = ""
+    let q08 : String = ""
+    let q09 : String = ""
+    let q10 : String = ""
 }
 
-let op1 = suggestions(choices: 1111111111)
-//let op1 = suggestions(choices: 1111111111)
-//let op2 = suggestions(choices: 1111111112)
-//let op3 = suggestions(choices: 1111111113)
-//let op4 = suggestions(choices: 1111111114)
+let r01 : String = "Rest 01"
+let r02 : String = "Rest 02"
+let r03 : String = "Rest 03"
+let r04 : String = "Rest 04"
+let r05 : String = "Rest 05"
+let r06 : String = "Rest 06"
+let r07 : String = "Rest 07"
+let r08 : String = "Rest 08"
+let r09 : String = "Rest 09"
+let r10 : String = "Rest 10"
 
+let pri = [r01, r02, r03, r04, r05, r06, r07, r08, r09, r10]
+        
 
+let op01 = 1111111111
+let op02 = 1111111112
+let op03 = 1111111113
+let op04 = 1111111114
+let op05 = 1111111115
+let op06 = 1111111116
+let op07 = 1111111117
+let op08 = 1111111118
+let op09 = 1111111119
+let op10 = 1111111110
 
-let suggestions = [op1]
+let aux = [op01, op02, op03, op04, op05, op06, op07, op08, op09, op10]
 
-//, op2, op3, op4]
+ 
+
--- ocinator/main.swift
+++ ocinator/main.swift
@@ -1,30 +1,29 @@
 import Foundation
 
-// IMPORT FAKE DATABASE
+    let u = Int(readLine(strippingNewline: true)!)!
 
-if let filepath = Bundle.main.path(forResource: "testFile", ofType: "swift") {
-    do {
-        let contents = try String(contentsOfFile: filepath)
-        print(contents)
+    if u == 1 {
+        let a = Int(readLine(strippingNewline: true)!)!
+        let b = Int(readLine(strippingNewline: true)!)!
+        let c = Int(readLine(strippingNewline: true)!)!
+        let d = Int(readLine(strippingNewline: true)!)!
+        let e = Int(readLine(strippingNewline: true)!)!
+        let f = Int(readLine(strippingNewline: true)!)!
+        let g = Int(readLine(strippingNewline: true)!)!
+        let h = Int(readLine(strippingNewline: true)!)!
+        let i = Int(readLine(strippingNewline: true)!)!
+        let j = Int(readLine(strippingNewline: true)!)!
+
+        let z = Int("\(a)\(b)\(c)\(d)\(e)\(f)\(g)\(h)\(i)\(j)")
+
+        if aux.contains(z!) {
+            let i1 = aux.firstIndex(where: {$0 == z!})
+            print("\(pri[i1!])")
+        }
     }
-}
 
-let a = Int(readLine(strippingNewline: true)!)!
-let b = Int(readLine(strippingNewline: true)!)!
-let c = Int(readLine(strippingNewline: true)!)!
-let d = Int(readLine(strippingNewline: true)!)!
-let e = Int(readLine(strippingNewline: true)!)!
-let f = Int(readLine(strippingNewline: true)!)!
-let g = Int(readLine(strippingNewline: true)!)!
-let h = Int(readLine(strippingNewline: true)!)!
-let i = Int(readLine(strippingNewline: true)!)!
-let j = Int(readLine(strippingNewline: true)!)!
 
-var z = Int("\(a)\(b)\(c)\(d)\(e)\(f)\(g)\(h)\(i)\(j)")
 
-let x = 0
 
-if students.contains(z!) {
-    print(students)
-}
+
 
